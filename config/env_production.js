132aca7f082d7f84e6efa8df71d02bb2e67e4979
Josh Miller <notjoshmiller@gmail.com>
2017-10-18 12:52:11 -0500

    Bump picker to 0.11.1, client to 0.5.13

--- config/env_production.js
+++ config/env_production.js
@@ -2,7 +2,7 @@ const DEFAULT_CNAME = 'filestackapi.com';
 
 const envGetter = (cname) => {
   return {
-    picker: `https://static.${cname || DEFAULT_CNAME}/picker/v3/picker-0.11.0.js`,
+    picker: `https://static.${cname || DEFAULT_CNAME}/picker/v3/picker-0.11.1.js`,
   };
 };
 
