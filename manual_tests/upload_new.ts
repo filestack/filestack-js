738e161372d91991e82a0cfe17b8f046440af979
pcholuj <piotr.choluj@filestack.com>
2019-04-26 15:32:31 +0200

    refactor(manual_test): update manual test file

--- manual_tests/upload_new.ts
+++ manual_tests/upload_new.ts
@@ -1,3 +1,4 @@
+
 /*
  * Copyright (c) 2018 by Filestack.
  * Some rights reserved.
@@ -17,20 +18,40 @@
 
 import { Client } from './../src/lib/client';
 import * as Path from 'path';
+import { S3Uploader } from './../src/lib/api/upload/uploaders/s3';
+import { getFile } from '../src/lib/api/upload';
 
-const fs = new Client('APEkwxKMZTsWNIP0XQsv2z');
+// const fs = new Client('APEkwxKMZTsWNIP0XQsv2z');
 
-fs.multiupload([
-  {
-    file: Path.resolve(__dirname, './manual_10mb.mp4'),
-    name: 'test.mp4',
-  },
-], {}, {
-  filename: () => 'test2.mp4',
-}).then((res) => {
-  console.dir(res, { depth: null });
-});
+// fs.multiupload(
+//   [
+//     {
+//       file: Path.resolve(__dirname, './manual_10mb.mp4'),
+//       name: 'test.mp4',
+//     },
+//   ],
+//   {},
+//   {
+//     filename: () => 'test2.mp4',
+//   }
+// ).then(res => {
+//   console.dir(res, { depth: null });
+// });
 
 // up.multiupload([Path.resolve(__dirname, './upload.js'), Path.resolve(__dirname, './manual_10mb.mp4')]).then((res) => {
 //   console.info('Upload done!', res);
 // });
+
+(async () => {
+  const file = await getFile(Path.resolve(__dirname, './manual_10mb.mp4'));
+  const u = new S3Uploader({});
+  u.setHost('https://upload.filestackapi.com');
+  u.setApikey('APEkwxKMZTsWNIP0XQsv2z');
+  u.addFile(file);
+
+  const res = await u.execute().catch((e) => {
+    console.log('ERROR', e);
+  });
+
+  console.log(res);
+})();
