4c730134a9901637153944fa5257a7f5908b4353
pcholuj <piotr.choluj@filestack.com>
2019-05-14 09:46:59 +0200

    test(security): missing test file

---
+++ src/lib/api/security.spec.browser.ts
@@ -0,0 +1,38 @@
+/*
+ * Copyright (c) 2019 by Filestack.
+ * Some rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the 'License');
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an 'AS IS' BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { getSecurity } from './security';
+import * as utils from './../utils';
+
+describe('api/security', () => {
+  beforeAll(() => {
+    spyOn(utils, 'isNode').and.returnValue(false);
+  });
+
+  describe('getSecurity', () => {
+    it('should throw not supported error', () => {
+      const policy = {
+        expiry: 1523595600,
+        call: ['pick', 'read', 'stat', 'write', 'writeUrl', 'store', 'convert', 'remove', 'exif', 'runWorkflow'],
+        handle: 'TEST_HANDLE',
+      };
+      const appSecret = 'testAppSecret';
+
+      expect(() => getSecurity(policy, appSecret)).toThrowError('getSecurity is only supported in nodejs');
+    });
+  });
+});
