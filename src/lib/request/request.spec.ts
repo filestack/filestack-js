5e8867b3c85b4d61d1210f5ddf15d781e01b2619
MowiliMi <38068391+MowiliMi@users.noreply.github.com>
2019-12-02 15:11:16 +0100

    working progress unit test cdn.

---
+++ src/lib/request/request.spec.ts
@@ -0,0 +1,34 @@
+/*
+ * Copyright (c) 2019 by Filestack.
+ * Some rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { FsRequest } from './request';
+import { FsRequestOptions } from './types';
+
+describe('Request/Request', () => {
+  describe('new FsRequest()', () => {
+    const request = new FsRequest({});
+    const expected = { defaults: {}, dispatcher: { adapter: { redirectHoops: 0, redirectPaths: [] } } };
+    it(`request should return equal to `, () => expect(request).toEqual(expected));
+  });
+
+  describe('get', () => {
+    it(`FsRequest get method should return status 200`, async () => {
+      const response = await FsRequest.get('https://filestack.com');
+      expect(response.status).toBe(200);
+    });
+  });
+});
