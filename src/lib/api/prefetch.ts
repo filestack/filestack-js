ff5c6b00e45f6cebdc1bed2e273f02863602895a
MowiliMi <38068391+MowiliMi@users.noreply.github.com>
2020-02-17 09:47:56 +0100

    prefetch: config, reassign, cleanup

--- src/lib/api/prefetch.ts
+++ src/lib/api/prefetch.ts
@@ -19,10 +19,34 @@ import { ClientOptions, Session } from '../client';
 import { PickerOptions } from './../picker';
 import { requestWithSource } from '../api/request';
 
+type PrefetchOptionsSetting = {
+  inapp_browser: boolean;
+  customsource: boolean;
+};
+
+type PrefetchOptionsPermissions = {
+  intelligent_ingestion: boolean;
+  blocked: boolean;
+  blacklisted: boolean;
+  whitelabel: boolean;
+};
+
 type PrefetchOptions = {
-  pickerOptions: PickerOptions,
-  settings: any, // @todo interface
-  permissions: any,  // @todo interface
+  pickerOptions: PickerOptions;
+  settings: PrefetchOptionsSetting;
+  permissions: PrefetchOptionsPermissions;
+};
+
+type responseObject = {
+  intelligent_ingestion: boolean;
+  blocked: boolean;
+  blacklisted: boolean;
+  whitelabel: boolean;
+  customsource: boolean;
+  inapp_browser: boolean;
+  updated_config: {
+    fromSources: string[];
+  };
 };
 
 /**
@@ -35,59 +59,84 @@ export class Prefetch {
 
   private configToCheck: PickerOptions;
 
-  // private requestInProgress;
-
   constructor(session: Session) {
     this.session = session;
     this.prefetchUrl = session.urls.cloudApiUrl;
   }
 
   async getConfig({ pickerOptions, settings, permissions }: PrefetchOptions) {
-
+    // @todo: stay?
     if (this.session.prefetch) {
       return Promise.resolve(this.session.prefetch);
     }
 
     const configToSend = this.cleanUpCallback(pickerOptions);
-    //pickerOptions
 
-    // const params = {
-    //   apikey: this.session.apikey,
-    //   security: {
-    //     policy: this.session.policy,
-    //     signature: this.session.signature,
-    //   },
-    //   settings,
-    //   permissions,
-    //   picker_config: configToSend,
-    // };
+    const paramsToSend = {
+      apikey: this.session.apikey,
+      security: {
+        policy: this.session.policy,
+        signature: this.session.signature,
+      },
+      settings,
+      permissions,
+      picker_config: configToSend,
+    };
+
+    const response = await requestWithSource()
+      .post(`${this.prefetchUrl}/prefetch`, paramsToSend)
+      .then(res => res.data)
+      .then(data => data);
 
     // @todo
-    // after response reassign callback and return picker confif
+    // after response reassign callback and return picker config
+    // const pickerOptionsToReturn = this.reassignCallbacks(response);
 
-    // cosnt pickerOptionsToReturn = this.reassignCallbacks(response);
-
-    return Promise.resolve({
-      blocked: false,
-      settings: {
-        inapp_browser: true,
-      },
-      permissions: {
-        whitelabel: false,
-      },
-      pickerOptions,
-    });
+    const pickerOptionsToReturn = this.reassignCallbacks(response);
+    return pickerOptionsToReturn;
 
     // @todo make request to backend and assign them to session
     // return Promise.resolve(this.session.prefetch);
   }
 
   private cleanUpCallback(pickerOptions: PickerOptions) {
-    this.configToCheck = pickerOptions;
     // make copy of original picker config and cleanup all callbacks (all methods started form onXX)
+    const tempCallbackVariable: null = null;
+
+    this.configToCheck = pickerOptions;
+
+    pickerOptions = {
+      onClose: tempCallbackVariable,
+      onOpen: tempCallbackVariable,
+      onFileSelected: tempCallbackVariable,
+      onFileUploadStarted: tempCallbackVariable,
+      onFileUploadFinished: tempCallbackVariable,
+      onFileUploadFailed: tempCallbackVariable,
+      onFileUploadProgress: tempCallbackVariable,
+      onFileCropped: tempCallbackVariable,
+      onUploadStarted: tempCallbackVariable,
+      onUploadDone: tempCallbackVariable,
+    };
+
+    return pickerOptions;
   }
 
-  private reassignCallbacks() {
+  private reassignCallbacks(response: responseObject): PrefetchOptions {
     // reassign all callback to new config returned from prefetch
+    return {
+      pickerOptions: {
+        fromSources: response.updated_config.fromSources,
+      },
+      settings: {
+        inapp_browser: response.inapp_browser,
+        customsource: response.customsource,
+      },
+      permissions: {
+        intelligent_ingestion: response.intelligent_ingestion,
+        blocked: response.blocked,
+        blacklisted: response.blacklisted,
+        whitelabel: response.whitelabel,
+      },
+    };
   }
 }
