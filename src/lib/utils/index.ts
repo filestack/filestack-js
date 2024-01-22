c5c9548011412d4f275d99b2ba394aafc8dc7017
gary-singh-filestack <gary.singh@filestack.com>
2024-01-22 15:33:55 +0530

    update picker

--- src/lib/utils/index.ts
+++ src/lib/utils/index.ts
@@ -148,7 +148,7 @@ export const getMimetype = async(file: Uint8Array | Buffer, name?: string): Prom
  * @return string|boolean
  */
 export const extensionToMime = (ext: string) => {
-  console.log("getting mimetype : ",ext);
+  console.log("getting mimetype: ",ext);
   if (!ext || ext.length === 0) {
     return;
   }
