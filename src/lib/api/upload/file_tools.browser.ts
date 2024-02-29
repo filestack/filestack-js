de08f023efe8a2091ba2231c35cb8c89e4babb97
gary-singh-filestack <gary.singh@filestack.com>
2024-02-29 19:05:09 +0530

    add log

--- src/lib/api/upload/file_tools.browser.ts
+++ src/lib/api/upload/file_tools.browser.ts
@@ -158,7 +158,7 @@ export const getFile = (input: InputFile, sanitizeOptions?: SanitizeOptions): Pr
   let filename;
   let file: Blob;
 
-  console.log('input file: ',input);
+  console.log('inputfile: ',input);
   if (isFileNamed(input)) {
     filename = input.name;
     input = input.file;
