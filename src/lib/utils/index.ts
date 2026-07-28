6df3ee6d9e7228bb68dfa98ab923195fef0a6a3e
kuldeep-filestack <kuldeep.chander@filestack.com>
2026-07-28 13:46:18 +0530

    FS-13279 : strip directory path from filename in sanitizeName

--- src/lib/utils/index.ts
+++ src/lib/utils/index.ts
@@ -208,6 +208,12 @@ export const sanitizeName = (name: string, options: SanitizeOptions = true): str
     return 'undefined';
   }
 
+  name = name.split('/').pop();
+
+  if (name.length === 0) {
+    return 'undefined';
+  }
+
   const fileParts = name.split('.');
 
   if (fileParts.length > 1) {
