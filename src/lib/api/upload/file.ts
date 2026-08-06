31c83f2d6b572ebcc0d6633f6b0457392de33223
kuldeep-filestack <kuldeep.chander@filestack.com>
2026-08-06 16:38:57 +0530

    revert: [FS-13279] undo SDK filename path strip;fix moved to filestack-uploads

--- src/lib/api/upload/file.ts
+++ src/lib/api/upload/file.ts
@@ -124,7 +124,7 @@ export class File {
   public set customName(val: ((file: this) => string) | string) {
     switch (typeof val) {
       case 'string':
-        this.name = val.split('/').pop();
+        this.name = val;
         break;
       case 'function':
         const newName = val(this);
@@ -132,7 +132,7 @@ export class File {
           throw new Error(`Name function must return a string. Current return type is ${typeof val}`);
         }
 
-        this.name = newName.split('/').pop();
+        this.name = val(this);
         break;
     }
   }
